f4b5e18be6017fa84f4777dc431af81ccd9720c7
enikonagy2005 <83341636+enikonagy2005@users.noreply.github.com>
2022-12-13 10:57:37 +0100

    valaszcheck

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function szamol() {
-    var x = document.getElementById("szam").value;
-    document.getElementById("ide_irj").innerHTML = x*9
+function elsovalasz() {
+    var userValasz = document.getElementById("valastEgy").value;
+    var leaderValasz = "Aatrox";
+    if (userValasz == leaderValasz) {
+    document.getElementById("ide_irj").innerHTML = "Gratulálunk! Kitaláltad a karaktert! Mehetsz a következő kérdésre:)";
+    }
+    else {
+    document.getElementById("ide_irj").innerHTML = "Sajnálom, a válasz nem helyes. Próbáld újra!";
+    }
     return false
 }
