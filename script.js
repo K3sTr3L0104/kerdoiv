a4bb0e061a367ca401906920fb54a55ff93478fb
enikonagy2005 <83341636+enikonagy2005@users.noreply.github.com>
2022-12-13 11:19:22 +0100

    valasz3 plusz kerdes

--- script.js
+++ script.js
@@ -9,14 +9,29 @@ function elsovalasz() {
     }
     return false
 }
-function masodikValasz() {
-    var userValasz1 = document.getElementById("valaszKetto").value;
-    var leaderValasz1 = "Fizz";
-    if (userValasz1 == leaderValasz1) {
-    document.getElementById("ValaszMegjelenites").innerHTML = "Gratulálunk! Kitaláltad a karaktert! Mehetsz a következő kérdésre:)";
+
+
+    function masodikvalasz() {
+        var userValasz = document.getElementById("valastKetto").value;
+        var leaderValasz = "Aatrox";
+        if (userValasz == leaderValasz) {
+        document.getElementById("ide_irj").innerHTML = "Gratulálunk! Kitaláltad a karaktert! Mehetsz a következő kérdésre:)";
+        }
+        else {
+        document.getElementById("ide_irj").innerHTML = "Sajnálom, a válasz nem helyes. Próbáld újra!";
+        }
+        return false
     }
-    else {
-    document.getElementById("ValaszMegjelenites").innerHTML = "Sajnálom, a válasz nem helyes. Próbáld újra!";
+
+
+        function harmadikvalasz() {
+            var userValasz = document.getElementById("valastHarom").value;
+            var leaderValasz = "Hecarim";
+            if (userValasz == leaderValasz) {
+            document.getElementById("valasz3").innerHTML = "Gratulálunk! Kitaláltad a karaktert! Mehetsz a következő kérdésre:)";
+            }
+            else {
+            document.getElementById("valasz3").innerHTML = "Sajnálom, a válasz nem helyes. Próbáld újra!";
+            }
+            return false
     }
-    return false
-}
