fe32b772ea79de5ad379a806b28e0bd9eec0e4d3
K3sTr3L0104 <84494408+K3sTr3L0104@users.noreply.github.com>
2022-12-13 11:10:17 +0100

    masodikKerdes

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ function elsovalasz() {
     return false
 }
 function masodikValasz() {
-    var userValasz = document.getElementById("valaszKetto").value;
-    var leaderValasz = "Fizz";
-    if (userValasz == leaderValasz) {
-    document.getElementById("ide_irj").innerHTML = "Gratulálunk! Kitaláltad a karaktert! Mehetsz a következő kérdésre:)";
+    var userValasz1 = document.getElementById("valaszKetto").value;
+    var leaderValasz1 = "Fizz";
+    if (userValasz1 == leaderValasz1) {
+    document.getElementById("ValaszMegjelenites").innerHTML = "Gratulálunk! Kitaláltad a karaktert! Mehetsz a következő kérdésre:)";
     }
     else {
-    document.getElementById("ide_irj").innerHTML = "Sajnálom, a válasz nem helyes. Próbáld újra!";
+    document.getElementById("ValaszMegjelenites").innerHTML = "Sajnálom, a válasz nem helyes. Próbáld újra!";
     }
     return false
 }
